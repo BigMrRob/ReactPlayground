653478c05219a5275aebf7462d23c6ae5f327ae4
Josh Harrington <jlharrington10@Joshs-MacBook-Air.local>
2022-08-19 15:33:28 -0500

    Base changes for initial form setup

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,35 @@ import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Build your form here</h1>
+    <div className={styles.PageWrapper}>
+      <form className={styles.Form}>
+        <div className={styles.FormInputWrapper}>
+          <input className={styles.FormInput} placeholder="First Name"></input>
+        </div>
+        <div className={styles.FormInputWrapper}>
+          <input className={styles.FormInput}></input>
+        </div>
+        <div className={styles.FormInputWrapper}>
+          <input className={styles.FormInput}></input>
+        </div>
+        <div className={styles.FormInputWrapper}>
+          <input className={styles.FormInput}></input>
+        </div>
+      </form>
+      <div className={styles.ButtonWrapper}>
+        <button className={styles.ButtonCancel}>Cancel</button>
+        <button className={styles.ButtonSignUp}>Sign Up</button>
+      </div>
+
+      {/* <h1>Build your form here</h1>
       <h2>Define the styles in Home.module.scss</h2>
 
-      {/* The following is how you define styles in React vs. regular HTML using SCSS Modules. Just follow the convention for now I will explain more later */}
       <div className={styles.Example}>
         <h2 className={styles.ExampleHeader}>This is an example header</h2>
         <div className={styles.ExampleBox}>
           <p className={styles.ExampleBoxDetails}>These are example details</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
