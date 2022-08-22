3f2ba0966dc1e36de649e81a3d25f4d5b9c4271f
Robert B. Farris III <54653488+BigMrRob@users.noreply.github.com>
2022-08-22 10:47:18 -0500

    Delete components directory

--- components/Form/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../../styles/components/Form.module.scss";
-
-const Form = ({ children }) => {
-  return <form className={styles.FormWrapper}>{children}</form>;
-};
-
-export default Form;
